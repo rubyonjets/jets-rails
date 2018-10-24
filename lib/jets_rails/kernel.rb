d659a38cade3a404cc6bcc4e29dea37e868a5d3f
Tung Nguyen <tongueroo@gmail.com>
2018-10-24 14:56:00 +0000

    degugging, write to jets-output directly

--- lib/jets_rails/kernel.rb
+++ lib/jets_rails/kernel.rb
@@ -30,7 +30,7 @@ module Kernel
       def #{meth}(*args, &block)
         @@io_buffer << args.first # message
         # Write immediately for debugging
-        IO.write("/tmp/rack-output.log", args.first.to_s + "\n", mode: 'a')
+        IO.write("/tmp/jets-output.log", args.first.to_s + "\n", mode: 'a')
         original_#{meth}(*args, &block)
       end
     CODE
@@ -44,15 +44,15 @@ module Kernel
   #   jets/lib/jets/core_ext/kernel.rb:20:in `write': "\x89" from ASCII-8BIT to UTF-8 (Encoding::UndefinedConversionError)
   # Rescue and discard it to keep the process alive.
   def io_flush
-    chunk = @@io_buffer.join("\n")
-    chunk += "\n" unless chunk == ''
-    begin
-      # since we always append to the file, the node shim is responsible for truncating the file
-      IO.write(JETS_OUTPUT, chunk, mode: 'a') if chunk
-    # Writing to log with binary content will crash the process so rescuing it and writing an info message.
-    rescue Encoding::UndefinedConversionError
-      IO.write(JETS_OUTPUT, "[BINARY DATA]\n", mode: 'a')
-    end
-    @@io_buffer = []
+    # chunk = @@io_buffer.join("\n")
+    # chunk += "\n" unless chunk == ''
+    # begin
+    #   # since we always append to the file, the node shim is responsible for truncating the file
+    #   IO.write(JETS_OUTPUT, chunk, mode: 'a') if chunk
+    # # Writing to log with binary content will crash the process so rescuing it and writing an info message.
+    # rescue Encoding::UndefinedConversionError
+    #   IO.write(JETS_OUTPUT, "[BINARY DATA]\n", mode: 'a')
+    # end
+    # @@io_buffer = []
   end
 end
