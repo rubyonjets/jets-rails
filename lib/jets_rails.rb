1391750f79cc6ee4673c3798fc7c596d99104402
Tung Nguyen <tongueroo@gmail.com>
2018-10-24 15:09:53 +0000

    simplify implementation by writing to disk immediately

--- lib/jets_rails.rb
+++ lib/jets_rails.rb
@@ -6,43 +6,6 @@
 #
 #   JetsRails.stage = ENV['JETS_STAGE'] || 'dev'
 #
-# = Signal Trapping
-#
-# Puma uses a lot of different signals
-# https://github.com/puma/puma/blob/master/docs/signals.md
-#
-# TTIN increment the worker count by 1
-# TTOU decrement the worker count by 1
-# TERM send TERM to worker. Worker will attempt to finish then exit.
-# USR2 restart workers. This also reloads puma configuration file, if there is one.
-# USR1 restart workers in phases, a rolling restart. This will not reload configuration file.
-# HUP reopen log files defined in stdout_redirect configuration parameter. If there is no stdout_redirect option provided it will behave like INT
-# INT equivalent of sending Ctrl-C to cluster. Will attempt to finish then exit.
-# CHLD
-#
-# All signals:
-#
-# $ kill -l
-# 1) SIGHUP       2) SIGINT       3) SIGQUIT      4) SIGILL       5) SIGTRAP
-# 6) SIGABRT      7) SIGBUS       8) SIGFPE       9) SIGKILL     10) SIGUSR1
-# 11) SIGSEGV     12) SIGUSR2     13) SIGPIPE     14) SIGALRM     15) SIGTERM
-# 16) SIGSTKFLT   17) SIGCHLD     18) SIGCONT     19) SIGSTOP     20) SIGTSTP
-# 21) SIGTTIN     22) SIGTTOU     23) SIGURG      24) SIGXCPU     25) SIGXFSZ
-# 26) SIGVTALRM   27) SIGPROF     28) SIGWINCH    29) SIGIO       30) SIGPWR
-# 31) SIGSYS      34) SIGRTMIN    35) SIGRTMIN+1  36) SIGRTMIN+2  37) SIGRTMIN+3
-# 38) SIGRTMIN+4  39) SIGRTMIN+5  40) SIGRTMIN+6  41) SIGRTMIN+7  42) SIGRTMIN+8
-# 43) SIGRTMIN+9  44) SIGRTMIN+10 45) SIGRTMIN+11 46) SIGRTMIN+12 47) SIGRTMIN+13
-# 48) SIGRTMIN+14 49) SIGRTMIN+15 50) SIGRTMAX-14 51) SIGRTMAX-13 52) SIGRTMAX-12
-# 53) SIGRTMAX-11 54) SIGRTMAX-10 55) SIGRTMAX-9  56) SIGRTMAX-8  57) SIGRTMAX-7
-# 58) SIGRTMAX-6  59) SIGRTMAX-5  60) SIGRTMAX-4  61) SIGRTMAX-3  62) SIGRTMAX-2
-# 63) SIGRTMAX-1  64) SIGRTMAX
-#
-# Using a signal that is not used by the puma webserver.
-#
-Signal.trap("IO") do # kill -29
-  JetsRails::IO.flush
-end
-
 module JetsRails
   cattr_accessor :stage
   self.stage = "dev" # default. should be set to in Rails initializer
@@ -50,6 +13,5 @@ module JetsRails
   autoload :StageMiddleware, 'jets_rails/stage_middleware'
 end
 
-require "jets_rails/io"
-require "jets_rails/railtie"
 require "jets_rails/kernel"
+require "jets_rails/railtie"
--- lib/jets_rails/kernel.rb
+++ lib/jets_rails/kernel.rb
@@ -1,9 +1,7 @@
-# Works with io.rb
-# IO.flush # flush output and write to disk for node shim
+# With this implementation we write to disk immedaitely. This simplifies the
+# implementation because we do not have to flush to io buffer by passing signals
+# back and forth from the jets ruby server process to the rack server process.
 module Kernel
-  JETS_OUTPUT = "/tmp/jets-output.log"
-  @@io_buffer = []
-
   # List from https://ruby-doc.org/core-2.5.1/Kernel.html
   # Note, will lose pp format in the @io_buffer but looks like a lot of work to keep the pp format.
   # Must override stdout which can be messy quick: https://www.ruby-forum.com/topic/43725
@@ -21,38 +19,17 @@ module Kernel
     #
     #   alias_method :original_puts, :puts
     #   def puts(*args, &block)
-    #     @@io_buffer << args.first # message
     #     original_puts(*args, &block)
     #   end
     #
     class_eval <<~CODE
       alias_method :original_#{meth}, :#{meth}
       def #{meth}(*args, &block)
-        @@io_buffer << args.first # message
         # Write immediately for debugging
-        IO.write("/tmp/jets-output.log", args.first.to_s + "\n", mode: 'a')
+        message = "Rails: " + args.first.to_s + "\n"
+        IO.write("/tmp/jets-output.log", message, mode: 'a')
         original_#{meth}(*args, &block)
       end
     CODE
-  end
-
-  def io_buffer
-    @@io_buffer
-  end
-
-  # Note: Writing binary data to the log will crash the process with an error like this:
-  #   jets/lib/jets/core_ext/kernel.rb:20:in `write': "\x89" from ASCII-8BIT to UTF-8 (Encoding::UndefinedConversionError)
-  # Rescue and discard it to keep the process alive.
-  def io_flush
-    # chunk = @@io_buffer.join("\n")
-    # chunk += "\n" unless chunk == ''
-    # begin
-    #   # since we always append to the file, the node shim is responsible for truncating the file
-    #   IO.write(JETS_OUTPUT, chunk, mode: 'a') if chunk
-    # # Writing to log with binary content will crash the process so rescuing it and writing an info message.
-    # rescue Encoding::UndefinedConversionError
-    #   IO.write(JETS_OUTPUT, "[BINARY DATA]\n", mode: 'a')
-    # end
-    # @@io_buffer = []
   end
 end
